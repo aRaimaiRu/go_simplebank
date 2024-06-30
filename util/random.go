fb7dd22deb315c3e3d4771afbacba8397395d537
Kittipat <first1801@hotmail.com>
2024-06-30 18:33:16 +0700

    initialize

---
+++ util/random.go
@@ -0,0 +1,39 @@
+package util
+
+import (
+	"math/rand"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyz"
+
+func init() {
+	rand.Seed((time.Now().UnixNano()))
+}
+
+// RandomInt generates a random integer between min and max
+func RandomInt(min, max int64) int64 {
+	return min + rand.Int63n(max-min+1)
+}
+
+func RandomString(length int) string {
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[rand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func RandomOwner() string {
+	return RandomString(6)
+}
+
+func RandomMoney() int64 {
+	return RandomInt(0, 1000)
+}
+
+func RandomCurrency() string {
+	currencies := []string{"USD", "EUR", "CAD"}
+	n := len(currencies)
+	return currencies[rand.Intn(n)]
+}
